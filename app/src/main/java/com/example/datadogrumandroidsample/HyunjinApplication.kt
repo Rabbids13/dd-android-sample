6bb4cc67ded1a6a839960a06590c32d0c8da9ad7
helmy zakiudin <helmyzakiudin@helmys-MacBook-Air.local>
2026-01-21 12:42:30 +0700

    First commit

---
+++ app/src/main/java/com/example/datadogrumandroidsample/HyunjinApplication.kt
@@ -0,0 +1,105 @@
+package com.example.datadogrumandroidsample
+
+import android.app.Application
+import android.util.Log
+import com.datadog.android.Datadog
+import com.datadog.android.DatadogSite
+import com.datadog.android.core.configuration.BatchSize
+import com.datadog.android.core.configuration.Configuration
+import com.datadog.android.core.configuration.UploadFrequency
+//import com.datadog.android.log.Logs
+//import com.datadog.android.log
+import com.datadog.android.privacy.TrackingConsent
+import com.datadog.android.rum.GlobalRumMonitor
+import com.datadog.android.rum.Rum
+import com.datadog.android.rum.RumConfiguration
+import com.datadog.android.rum.tracking.ActivityViewTrackingStrategy
+import com.datadog.android.sessionreplay.SessionReplay
+import com.datadog.android.sessionreplay.SessionReplayConfiguration
+import com.datadog.android.sessionreplay.SessionReplayPrivacy
+import com.datadog.android.trace.TraceConfiguration
+import com.datadog.android.trace.Trace
+import com.datadog.android.trace.AndroidTracer
+import io.opentracing.util.GlobalTracer
+
+class HyunjinApplication : Application() {
+	override fun onCreate() {
+
+		super.onCreate()
+		Log.i("rum_test", "oncreate")
+
+		initRum()
+
+		Log.i("rum_test", "init Complete")
+	}
+
+	private fun initRum() {
+        Datadog.setVerbosity(Log.VERBOSE)
+
+        val clientToken = "pubd6cd280cffccdbca312a9abb2b8400ae"
+        val applicationId = "adf89367-0027-48d9-b103-29a9063ccce0"
+        val environment = "stg"
+        val appVariant = "DatadogApp"
+
+        // 1. Initialize Datadog Core
+        val config = Configuration.Builder(
+            clientToken = clientToken,
+            env = environment,
+            variant = appVariant
+        )
+            .useSite(DatadogSite.US1)
+            .setBatchSize(BatchSize.SMALL)
+            .setUploadFrequency(UploadFrequency.FREQUENT)
+            .build()
+
+        Datadog.initialize(this, config, TrackingConsent.GRANTED)
+
+        // 2. Enable RUM
+        val rumConfig = RumConfiguration.Builder(applicationId)
+            .useViewTrackingStrategy(ActivityViewTrackingStrategy(true))
+            .trackUserInteractions()
+            .setTelemetrySampleRate(100f)
+            .build()
+
+        Rum.enable(rumConfig)
+
+        val sessionReplayConfig = SessionReplayConfiguration.Builder(100f)
+            .build()
+        SessionReplay.enable(sessionReplayConfig)
+
+//        // 3. Enable Logs
+//        Logs.enable(LogsConfiguration.Builder().build())
+
+        // Optional debug
+        GlobalRumMonitor.get().debug = true
+
+        Log.i("rum_test", "Datadog init complete")
+
+        val traceConfig = TraceConfiguration.Builder().build()
+        Trace.enable(traceConfig)
+
+        val tracer = AndroidTracer.Builder().build()
+        GlobalTracer.registerIfAbsent(tracer)
+
+//		val clientToken = "pubd6cd280cffccdbca312a9abb2b8400ae"
+//		val applicationId = "adf89367-0027-48d9-b103-29a9063ccce0"
+//
+//		val environmentName = "stg"
+//		val appVariantName = "DatadogApp"
+//
+//		val configuration = Configuration.Builder(
+//			logsEnabled = true,
+//			tracesEnabled = true,
+//			crashReportsEnabled = true,
+//			rumEnabled = true
+//		)
+//			.trackInteractions()
+//			.useSite(DatadogSite.US1)
+//			.build()
+//
+//		val credentials = Credentials(clientToken, environmentName, appVariantName, applicationId)
+//		Datadog.initialize(this, credentials, configuration, TrackingConsent.GRANTED)
+//		// Session 샘플레이트
+//		GlobalRum.registerIfAbsent(RumMonitor.Builder().sampleRumSessions(100.0f).build())
+	}
+}
---
+++ app/src/main/java/com/example/datadogrumandroidsample/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.datadogrumandroidsample
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.datadog.android.Datadog
+import com.datadog.android.okhttp.DatadogEventListener
+import com.datadog.android.okhttp.DatadogInterceptor
+import com.datadog.android.rum.GlobalRumMonitor
+import com.example.datadogrumandroidsample.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class MainActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityMainBinding
+
+	private var mRetrofit: Retrofit? = null
+
+	private var mRetrofitAPI: RetrofitAPI? = null
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+
+		binding = ActivityMainBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		initTransferButtonView()
+		initTimingButtonView()
+		//SetUser
+		Datadog.setUserInfo("1", "HyunjinLee", "hyunjin.lee@datadoghq.com")
+		initRetrofitInit()
+	}
+
+	private fun initTransferButtonView() {
+		binding.transfer.setOnClickListener {
+			val testString = mRetrofitAPI?.test()
+			testString?.enqueue(object : Callback<HyunjinResponse> {
+				override fun onResponse(call: Call<HyunjinResponse>, response: Response<HyunjinResponse>) {
+					Log.i("rum_test", "responseHeaders" + response.headers())
+					Log.i("rum_test", "responseBody" + response.body())
+				}
+
+				override fun onFailure(call: Call<HyunjinResponse>, t: Throwable) {
+					Log.i("rum_test", "onFail" + t)
+				}
+			})
+		}
+	}
+
+	private fun initTimingButtonView() {
+		binding.timing.setOnClickListener {
+			//버튼 클릭 시 Timing
+            GlobalRumMonitor.get().addTiming("initButton_load_timing")
+			//버튼 클릭 시 Global Context
+//            GlobalRumMonitor.a("paid", "2")
+		}
+	}
+
+	fun initRetrofitInit() {
+		//HTTP 인터셉터
+		val okHttpClient = OkHttpClient.Builder()
+			.addInterceptor(DatadogInterceptor(rumResourceAttributesProvider = CustomRumResourceAttributesProvider()))
+			.eventListenerFactory(DatadogEventListener.Factory())
+			.build()
+
+		mRetrofit =  Retrofit.Builder()
+			.client(okHttpClient)
+			.baseUrl("https://joongomarket.com/")
+			.addConverterFactory(GsonConverterFactory.create())
+			.build();
+
+		mRetrofitAPI = mRetrofit?.create(RetrofitAPI::class.java)
+	}
+
+	override fun onResume() {
+		super.onResume()
+		//onResume 시 로그 전송
+        GlobalRumMonitor.get().startView(this, "MainActivity", HashMap<String, String>().apply { put("onResume", "onResuemValue") })
+	}
+
+	override fun onPause() {
+		super.onPause()
+		//onPause 시 로그 전송
+        GlobalRumMonitor.get().stopView(this, HashMap<String, String>().apply { put("onPause", "onPauseValue") })
+	}
+
+}
